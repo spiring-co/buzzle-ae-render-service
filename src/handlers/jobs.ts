889a5fde52e989d4d73c25c84a0b2170a2cb529b
Harsh Bhatia <harshb.work@gmail.com>
2021-02-26 13:10:08 +0530

    Added Update functionality

--- src/handlers/jobs.ts
+++ src/handlers/jobs.ts
@@ -6,6 +6,7 @@ import { io } from "socket.io-client";
 import { render } from "@nexrender/core";
 import { ChildProcess } from "child_process";
 import toNexrenderJob from "../helpers/toNexrenderJob";
+import * as _ from "lodash";
 
 dotenv.config();
 const { API_URL, SOCKET_SERVER, API_TOKEN } = process.env;
@@ -28,81 +29,48 @@ const settings: any = {
 };
 
 const socketLogger = (id) => {
+  let logs = "";
   return {
     log: (data) => {
-      socket.emit("job-logs", id, data)
+      console.log(data)
+      logs += `${data} \n`;
+      socket.emit("job-logs", id, logs)
     }
   }
 }
 
 // returns true if consumed message successfully
 export default async function (data: any, eventType: string): Promise<boolean> {
-
-  let job;
+  // console.log(eventType, _.pick(data, ["updates", "extra"], data.job.id))
+  let { job, updates = false, extra = false } = data;
 
   switch (eventType) {
+
     case "created":
-      try {
-        // convert to nexrender job
-        job = toNexrenderJob(data.job);
-        settings.logger = socketLogger(job.uid)
-        // update status to started on API
-        await updateJob(job.uid, {
-          state: "started",
-          dateStarted: new Date().toISOString(),
-        });
-        socket.emit("job-progress", job, {
-          state: "started",
-        });
-
-        // TODO add socket implementation
-        job.onRenderProgress = (job, progress) => {
-          socket.emit("job-progress", job, {
-            state: "Rendering",
-            progress,
-          });
-        };
-
-        job = await render(job, settings);
-
-        // update output and status on API
-        await updateJob(job.uid, {
-          output: { label: "new", src: job.output },
-          state: "finished",
-          dateFinished: new Date().toISOString(),
-        });
-
-        socket.emit("job-progress", job, {
-          state: "finished",
-        });
-
-      } catch (e) {
-        await updateJob(job.uid, {
-          state: "error",
-          dateFinished: new Date().toISOString(),
-          failureReason: e.message,
-        });
-        socket.emit("job-progress", job, {
-          state: "error",
-        });
-        console.log(e);
-      } finally {
-        const logPath = `${path.join(process.cwd(), "renders")}/aerender-${job.uid
-          }.log`;
-        // update logs
-        if (fs.existsSync(logPath)) {
-          await updateJob(job.uid, {
-            logs: { label: "ae", text: fs.readFileSync(logPath, "utf8") },
-          });
-        }
-
-        runningInstance = null;
-        currentJob = null;
-      }
-
-      return true;
+      return await renderJob(job)
 
     case "updated":
+      console.log(updates, extra)
+      if (extra?.forceRerender) {
+        socketLogger(job.id).log("Rerendering as forced rerender flag")
+        return await renderJob(job)
+      }
+
+      if (_.has(updates, "data")) {
+        socketLogger(job.id).log("Rerendering as data has changed")
+        return await renderJob(job)
+      }
+
+      if (_.has(updates, "actions")) {
+        console.log("Rerendering as actions has changed")
+        return await renderJob(job)
+      }
+
+      if (_.has(updates, "renderPrefs")) {
+        console.log("Rerendering as renderPrefs has changed")
+        return await renderJob(job)
+      }
+
       break;
 
     case "deleted":
@@ -125,3 +93,66 @@ const updateJob = async (uid, body) => {
   const result = await res.json();
   console.log(result)
 };
+
+
+const renderJob = async (job) => {
+  try {
+    // convert to nexrender job
+    job = toNexrenderJob(job);
+    settings.logger = socketLogger(job.uid)
+    // update status to started on API
+    await updateJob(job.uid, {
+      state: "started",
+      dateStarted: new Date().toISOString(),
+    });
+    socket.emit("job-progress", job, {
+      state: "started",
+    });
+
+    // TODO add socket implementation
+    job.onRenderProgress = (job, progress) => {
+      socket.emit("job-progress", job, {
+        state: "Rendering",
+        progress,
+      });
+    };
+
+    job = await render(job, settings);
+
+    // update output and status on API
+    await updateJob(job.uid, {
+      output: { label: "new", src: job.output },
+      state: "finished",
+      dateFinished: new Date().toISOString(),
+    });
+
+    socket.emit("job-progress", job, {
+      state: "finished",
+    });
+
+  } catch (e) {
+    await updateJob(job.uid, {
+      state: "error",
+      dateFinished: new Date().toISOString(),
+      failureReason: e.message,
+    });
+    socket.emit("job-progress", job, {
+      state: "error",
+    });
+    console.log(e);
+  } finally {
+    const logPath = `${path.join(process.cwd(), "renders")}/aerender-${job.uid
+      }.log`;
+    // update logs
+    if (fs.existsSync(logPath)) {
+      await updateJob(job.uid, {
+        logs: { label: "ae", text: fs.readFileSync(logPath, "utf8") },
+      });
+    }
+
+    runningInstance = null;
+    currentJob = null;
+  }
+
+  return true;
+}
